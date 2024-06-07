e500e20fc967b05c6d17f9837b27340bc43bd86d
elop123 <142408289+elop123@users.noreply.github.com>
2024-06-07 12:36:10 +0200

    songs

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function validateEmail() {
+    const emailInput = document.getElementById('email-input');
+    const emailValue = emailInput.value;
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+    if (emailPattern.test(emailValue)) {
+        errorMessage.style.display = 'none';
+        alert('Thank you for signing up!');
+        emailInput.value = ''; 
+    } else {
+        errorMessage.style.display = 'block';
+    }
+}
